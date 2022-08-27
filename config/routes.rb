599da054443594bc0d898b1c1ca217e9e369de00
EC2 Default User <ec2-user@ip-172-31-95-15.ec2.internal>
2022-08-27 08:56:40 +0000

    [up]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,55 +1,35 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'category_motifs/index'
-    get 'category_motifs/edit'
-  end
-  namespace :admin do
-    get 'category_styles/index'
-    get 'category_styles/edit'
-  end
-  namespace :admin do
-    get 'category_media/index'
-    get 'category_media/edit'
-  end
-  namespace :admin do
-    get 'posts/index'
-    get 'posts/show'
-  end
-  namespace :admin do
-    get 'occupations/index'
-    get 'occupations/edit'
-  end
-  namespace :admin do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'posts/index'
-    get 'posts/new'
-    get 'posts/show'
-    get 'posts/edit'
-  end
-  namespace :public do
-    get 'users/my_page'
-    get 'users/quit'
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
-  end
-  namespace :public do
-    get 'homes/top'
-  end
-  get 'homes/top'
+
   devise_for :admin, controllers: {
         sessions: 'admin/sessions'
-      }
+  }
+
+  namespace :admin do
+    get "top" => "homes#top", as: "top"
+
+    patch "users/leave" => "users#leave", as: "leave"
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :occupations, only: [:index, :create, :edit, :update]
+    resources :posts, only: [:index, :show, :destroy]
+    resources :category_media, only: [:index, :create, :edit, :update]
+    resources :category_styles, only: [:index, :create, :edit, :update]
+    resources :category_motifs, only: [:index, :create, :edit, :update]
+  end
+
 
   devise_for :users, controllers: {
         sessions: 'public/sessions',
         registrations: 'public/registrations',
-      }
+  }
+
+  namespace :public do
+    root "homes#top", as: "top"
+    get "users/my_page" => "users#my_page", as: "my_page"
+    get "users/quit" => "users#quit", as: "quit"
+    patch "users/leave" => "users#leave", as: "leave"
+
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :posts
+  end
+
 end
