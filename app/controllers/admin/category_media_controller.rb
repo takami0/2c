c8743c33a75126d91d0ca56aaa65624d0e89631b
EC2 Default User <ec2-user@ip-172-31-95-15.ec2.internal>
2022-09-01 23:29:54 +0000

    [up]CategryMedium

--- app/controllers/admin/category_media_controller.rb
+++ app/controllers/admin/category_media_controller.rb
@@ -20,7 +20,7 @@ class Admin::CategoryMediaController < ApplicationController
 
   def update
     @medium = CategoryMedium.find(params[:id])
-    if @medium.update(category_medium_params)
+    if @medium.update!(category_medium_params)
       redirect_to admin_category_media_path
     else
       render :edit
