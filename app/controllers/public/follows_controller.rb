36a30f9504e6974a99991b7919824ddbbe2f3506
takami0 <tetorisu544@gmail.com>
2022-10-20 23:31:27 +0000

    [edit]ajax

--- app/controllers/public/follows_controller.rb
+++ app/controllers/public/follows_controller.rb
@@ -3,24 +3,31 @@ class Public::FollowsController < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-    follow = current_user.user_relationships.create(follow_user_id: params[:user_id])
-    if follow
-      notice = current_user.send_notifications.new(
+    follow = current_user.user_relationships.new(follow_user_id: params[:user_id])
+    follow_notice = current_user.send_notifications.new(
         send_user_id: current_user.id,
         received_user_id: params[:user_id],
         action: "follow"
         )
-      notice.save
-      #redirect_to public_user_path(params[:user_id])
+    respond_to do |format|
+      if follow.save
+        format.html { redirect_to public_user_path(params[:user_id]) }
+        format.js
+        follow_notice.save
+      end
     end
   end
 
   def destroy
     @user = User.find(params[:user_id])
-    unfollow = current_user.user_relationships.find_by(follow_user_id: params[:user_id]).destroy
-    if unfollow
-      current_user.send_notifications.find_by(received_user_id: params[:user_id], action: "follow").destroy
-      #redirect_to public_user_path(params[:user_id])
+    follow = current_user.user_relationships.find_by(follow_user_id: params[:user_id])
+    follow_notice = current_user.send_notifications.find_by(received_user_id: params[:user_id], action: "follow")
+    respond_to do |format|
+      if follow.destroy
+        format.html { redirect_to public_user_path(params[:user_id]) }
+        format.js
+        follow_notice.destroy
+      end
     end
   end
 
