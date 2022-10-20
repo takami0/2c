9ddd5720d8809079e9be3eb1dee536c6c9df44d7
takami0 <tetorisu544@gmail.com>
2022-10-20 12:49:58 +0000

    [edit]ajax

--- app/controllers/public/follows_controller.rb
+++ app/controllers/public/follows_controller.rb
@@ -2,6 +2,7 @@ class Public::FollowsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @user = User.find(params[:user_id])
     follow = current_user.user_relationships.create(follow_user_id: params[:user_id])
     if follow
       notice = current_user.send_notifications.new(
@@ -15,6 +16,7 @@ class Public::FollowsController < ApplicationController
   end
 
   def destroy
+    @user = User.find(params[:user_id])
     unfollow = current_user.user_relationships.find_by(follow_user_id: params[:user_id]).destroy
     if unfollow
       current_user.send_notifications.find_by(received_user_id: params[:user_id], action: "follow").destroy
