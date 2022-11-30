32ca1bc60561e82551ba197c98d181ef3ecc2e65
takami0 <tetorisu544@gmail.com>
2022-11-30 22:36:39 +0000

    [edit] public_followers, followings

--- app/controllers/public/followers_controller.rb
+++ app/controllers/public/followers_controller.rb
@@ -2,8 +2,10 @@ class Public::FollowersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    followers = User.find(params[:user_id]).followers
-    @followers = followers.where(valid_status: true).where.not(member_status: 2).where.not(display_status: false).page(params[:page]).per(10)
+    @user = User.find(params[:user_id])
+    
+    @followers = @user.followers.where(valid_status: true).where.not(member_status: 2).where.not(display_status: false).page(params[:page]).per(10)
   end
+
 
 end
--- app/controllers/public/followings_controller.rb
+++ app/controllers/public/followings_controller.rb
@@ -2,7 +2,7 @@ class Public::FollowingsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    followings = User.find(params[:user_id]).followings
-    @followings = followings.where(valid_status: true).where.not(member_status: 2).where.not(display_status: false).page(params[:page]).per(10)
+    @user = User.find(params[:user_id])
+    @followings = @user.followings.where(valid_status: true).where.not(member_status: 2).where.not(display_status: false).page(params[:page]).per(10)
   end
 end
