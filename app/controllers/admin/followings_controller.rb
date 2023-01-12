e9614b16c243a53ffd2accbf54b52d729cd38d2b
takami0 <tetorisu544@gmail.com>
2023-01-12 23:37:28 +0000

    [edit admin] user_show, followings

--- app/controllers/admin/followings_controller.rb
+++ app/controllers/admin/followings_controller.rb
@@ -1,7 +1,8 @@
 class Admin::FollowingsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
-    @followings = User.find(params[:user_id]).followings.page(params[:page]).per(10)
+    @user = User.find(params[:user_id])
+    @followings = @user.followings.page(params[:page]).per(10)
   end
 end
