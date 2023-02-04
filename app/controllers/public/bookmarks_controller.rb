de4aae49ec4d3f4e3c7917a841588a327ca467eb
takami0 <tetorisu544@gmail.com>
2023-02-04 03:47:09 +0000

    [up public_bookmark] change method of create

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -1,24 +1,19 @@
 class Public::BookmarksController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_post, only: [:create, :destroy]
+  before_action :find_post, only: [:destroy]
 
   def index
     @bookmarks = current_user.bookmarks.page(params[:page]).per(10)
   end
 
   def create
-    @bookmark = Bookmark.new(user_id: current_user.id, post_id: @post.id )
-    notice_bookmark = current_user.send_notifications.new(
-      send_user_id: current_user.id,
-      received_user_id: @post.user.id,
-      post_id: @post.id,
-      action: "bookmark"
-      )
-    respond_to do |format|
-      if @bookmark.save
-        format.html {redirect_to post_path(@post.id)}
+    @post = Post.find(params[:post_id])
+    if Bookmark.bookmarked(current_user, @post).blank?
+      Bookmark.create(user_id: current_user.id, post_id: @post.id )
+      Bookmark.generate_notification(current_user, @post)
+      respond_to do |format|
+        format.html {redirect_to post_path(_post.id)}
         format.js
-        notice_bookmark.save
       end
     end
   end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,19 +2,16 @@ class Bookmark < ApplicationRecord
   belongs_to :user
   belongs_to :post
   has_many :notifications, dependent: :destroy
-  
-  def create_notification
-    check = Notification.where(["send_user_id = ? and received_user_id = ? and post_id = ? and action = ?", current_user.id, @post.user_id, @post.id, "bookmark"]) 
-  end
-  
-   notice_bookmark = current_user.send_notifications.new(
+
+  scope :bookmarked, -> (current_user, _post) { where("user_id = ? and post_id = ?", current_user.id, _post.id) }
+
+  def self.generate_notification(current_user, _post)
+    notice = current_user.send_notifications.create(
       send_user_id: current_user.id,
-      received_user_id: @post.user.id,
-      post_id: @post.id,
+      received_user_id: _post.user.id,
+      post_id: _post.id,
       action: "bookmark"
       )
-  
-  # すでに「いいね」されているか検索
-    temp = Notification.where(["visitor_id = ? and visited_id = ? and post_id = ? and action = ? ", current_user.id, user_id, id, 'like'])
-      
+  end
+
 end
