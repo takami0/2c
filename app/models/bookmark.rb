a802f4127978cb9b9f1472e4f0a3303a00f4c533
takami0 <tetorisu544@gmail.com>
2023-02-05 10:03:21 +0000

    [edit public] change view of search_user

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -6,7 +6,7 @@ class Bookmark < ApplicationRecord
   scope :bookmarked, -> (current_user, _post) { where("user_id = ? and post_id = ?", current_user.id, _post.id) }
 
   def self.generate_notification(current_user, _post)
-    notice = current_user.send_notifications.create(
+    current_user.send_notifications.create(
       send_user_id: current_user.id,
       received_user_id: _post.user.id,
       post_id: _post.id,
