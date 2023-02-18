36946f24dfd75c67e416494012a6035a389703a7
takami0 <tetorisu544@gmail.com>
2023-02-18 02:59:49 +0000

    [public]edit controller of follow

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,17 +2,16 @@ class Follow < ApplicationRecord
   belongs_to :user
   belongs_to :follow_user, class_name: "User", foreign_key: "follow_user_id"
   has_many :notifications, dependent: :destroy
-  
-  def self.create_notification(current_user, _post)
-  end
 
-  def notice_follow
-    notice = current_user.send_notifications.new(
+  def self.create_notification(current_user, received_user)
+    current_user.send_notifications.create(
       send_user_id: current_user.id,
-      received_user_id: params[:user_id],
+      received_user_id: received_user.id,
       action: "follow"
       )
-    notice.save if Notification.valid?
+  end
+
+  def self.destroy_notification(current_user, received_user)
   end
 
   def notice_follow_delete
